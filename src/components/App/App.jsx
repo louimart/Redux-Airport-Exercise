9ad1da41658483a35ada5ac33f42dfe286b33ea8
Louis Martin <louimart@gmail.com>
2024-02-06 15:03:21 -0600

    redux setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,47 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function App() {
+  const [inputAirline, setInputAirline] = useState('');
+  const dispatch = useDispatch();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: 'ADD_AIRLINE',
+      payload: {
+        name: inputAirline,
+      },
+    });
+  };
+
+  const handleAddAirline = (event) => {
+    setInputAirline(event.target.value);
+  };
 
   return (
     <div>
       <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
-      <button>Add Airline</button>
+      <input
+        placeholder="Airline Name"
+        value={inputAirline}
+        onChange={handleAddAirline}
+      />
+      <button onClick={handleSubmit}>Add Airline</button>
       <table>{/* Airlines should be listed here */}</table>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Airline</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {inputAirline.map((element, itemIndex) => {
+              return <td>{inputAirline}</td>;
+            })}
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 
 /** TODO: import REDUX **/
 
-
 /** TODO: Add REDUCERS */
-
+function airlines(state = [], action) {
+  if (action.type === 'ADD_AIRLINE') {
+    const id = state.length;
+    const newAirline = {
+      id,
+      ...action.payload,
+    };
+    return [...state, newAirline];
+  }
+  return state;
+}
 
 /** TODO: Create store */
-
-
+const storeInstance = createStore(
+  combineReducers({ airlines }),
+  applyMiddleware(logger)
+);
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
